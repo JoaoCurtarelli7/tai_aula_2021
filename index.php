027f7c460f9a11765f9875605068370138318cb3
JoaoCurtarelli7 <joao.vcb@aluno.ifsc.edu.br>
2021-04-17 23:55:52 -0300

    atualizado

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include "./screens/head.php"
+?>
+
+<h4>Telas</h4>
+   
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" styles="max-width: 137px; margin-left: 74px; margin-top: 19px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Santos_logo.svg/1045px-Santos_logo.svg.png" alt="Imagem de capa do card">
+  <div class="card-body">
+    <h5 class="card-title">Título do card</h5>
+    <p class="card-text">Listagem</p>
+    <a href="./screens/UsuarioList.php" class="btn btn-primary">Ver</a>
+  </div>
+</div>
+    <?php
+include "./screens/footer.php"
+?>
